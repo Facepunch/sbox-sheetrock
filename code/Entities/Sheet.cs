c104b176caf8c07c8ddc9a03c5e09bb3a3fce410
Jake Evans <jacob.ee92@gmail.com>
2022-11-12 23:27:31 -0600

    Sheetrock brush entity

---
+++ code/Entities/Sheet.cs
@@ -0,0 +1,29 @@
+﻿
+[Library( "sheetrock_sheet" )]
+[Solid]
+[Display( Name = "Sheet", GroupName = "Materials", Description = "Sheetrock to cut sheets from" ), Category( "Tools" ), Icon( "landscape" )]
+[HammerEntity]
+internal class Sheet : BrushEntity, IUsable
+{
+
+	public Color GlowColor => Color.Orange;
+
+	public bool IsUsable( Entity user )
+	{
+		return true;
+	}
+
+	public bool OnUse( Entity user )
+	{
+		if ( user is not Pawn p ) 
+			return false;
+
+		if( IsClient )
+		{
+			Log.Error( "Enter sheetrock cutting mode" );
+		}
+
+		return false;
+	}
+
+}
