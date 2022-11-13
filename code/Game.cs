05e3ca5d87be279ac17a7962a8a9a9373d8e9db2
Jake Evans <jacob.ee92@gmail.com>
2022-11-12 19:01:33 -0600

    Map & basic game script

---
+++ code/Game.cs
@@ -0,0 +1,63 @@
+﻿using Sandbox;
+using Sandbox.UI.Construct;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+//
+// You don't need to put things in a namespace, but it doesn't hurt.
+//
+namespace Sandbox;
+
+/// <summary>
+/// This is your game class. This is an entity that is created serverside when
+/// the game starts, and is replicated to the client. 
+/// 
+/// You can use this to create things like HUDs and declare which player class
+/// to use for spawned players.
+/// </summary>
+public partial class MyGame : Sandbox.Game
+{
+
+	public override void FrameSimulate( Client cl )
+	{
+		base.FrameSimulate( cl );
+
+		var effects = Map.Camera.FindOrCreateHook<Sandbox.Effects.ScreenEffects>();
+		effects.FilmGrain.Intensity = 0.02f + 0.01f * 0.3f;
+		effects.FilmGrain.Response = 1.0f;
+		effects.Sharpen = 0.25f + 0.01f * 0.15f;
+		effects.Brightness = 0.5f;
+		effects.ChromaticAberration.Scale = 0.01f;
+		effects.Pixelation = 0.01f * 0.1f;
+		effects.Vignette.Intensity = 0.8f;
+		effects.Vignette.Roundness = 0.5f;
+		effects.Vignette.Smoothness = 1f;
+		effects.MotionBlur.Scale = .001f;
+		effects.MotionBlur.Samples = 10;
+	}
+
+	public override void ClientJoined( Client client )
+	{
+		base.ClientJoined( client );
+
+		// Create a pawn for this client to play with
+		var pawn = new Pawn();
+		client.Pawn = pawn;
+
+		// Get all of the spawnpoints
+		var spawnpoints = Entity.All.OfType<SpawnPoint>();
+
+		// chose a random one
+		var randomSpawnPoint = spawnpoints.OrderBy( x => Guid.NewGuid() ).FirstOrDefault();
+
+		// if it exists, place the pawn there
+		if ( randomSpawnPoint != null )
+		{
+			var tx = randomSpawnPoint.Transform;
+			tx.Position = tx.Position + Vector3.Up * 50.0f; // raise it up
+			pawn.Transform = tx;
+		}
+	}
+}
---
+++ code/Pawn.cs
@@ -0,0 +1,73 @@
+﻿
+using System;
+
+namespace Sandbox;
+
+partial class Pawn : Player
+{
+	/// <summary>
+	/// Called when the entity is first created 
+	/// </summary>
+	public override void Spawn()
+	{
+		base.Spawn();
+		
+		SetModel( "models/citizen/citizen.vmdl" );
+
+		Controller = new WalkController();
+		CameraMode = new FirstPersonCamera();
+		Animator = new StandardPlayerAnimator();
+
+		(Controller as WalkController).DefaultSpeed = 80;
+		
+		EnableHideInFirstPerson = true;
+	}
+
+	public override void PostCameraSetup( ref CameraSetup setup )
+	{
+		setup.ZNear = 0.1f;
+
+		base.PostCameraSetup( ref setup );
+
+		if ( setup.Viewer != null )
+		{
+			AddCameraEffects( ref setup );
+		}
+	}
+
+	float walkBob = 0;
+	float lean = 0;
+	float fov = 0;
+
+	private void AddCameraEffects( ref CameraSetup setup )
+	{
+		var speed = Velocity.Length.LerpInverse( 0, 320 );
+		var forwardspeed = Velocity.Normal.Dot( setup.Rotation.Forward );
+
+		var left = setup.Rotation.Left;
+		var up = setup.Rotation.Up;
+
+		if ( GroundEntity != null )
+		{
+			walkBob += Time.Delta * 25.0f * speed;
+		}
+
+		setup.Position += up * MathF.Sin( walkBob ) * speed * 2;
+		setup.Position += left * MathF.Sin( walkBob * 0.6f ) * speed * 1;
+
+		// Camera lean
+		lean = lean.LerpTo( Velocity.Dot( setup.Rotation.Right ) * 0.11f, Time.Delta * 15.0f );
+
+		var appliedLean = lean;
+		appliedLean += MathF.Sin( walkBob ) * speed * 0.3f;
+		setup.Rotation *= Rotation.From( 0, 0, appliedLean );
+
+		speed = (speed - 0.7f).Clamp( 0, 1 ) * 3.0f;
+
+		fov = fov.LerpTo( speed * 20 * MathF.Abs( forwardspeed ), Time.Delta * 4.0f );
+
+		setup.FieldOfView = 105;
+		setup.FieldOfView += fov;
+
+	}
+}
